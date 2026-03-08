022d5477496f849489855ce59d96b29182222436
moumita ghosh <ghoshmoumita9831@gmail.com>
2026-03-08 19:03:19 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text", {
+    strings: ["Backend Developer" , "Programmer" , "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
