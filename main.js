3a8b4928f1c9597d4cc774165871aca57364c369
moumita ghosh <ghoshmoumita9831@gmail.com>
2026-03-28 05:25:13 +0530

    add two eigth

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ var typed= new Typed(".text", {
     loop: true
 
 })
+
+window.addEventListener("scroll", function () {
+
+    let skills = document.querySelector(".skill-heading");
+    let position = skills.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight - 100;
+
+    if (position < screenPosition && position > 0) {
+        skills.classList.add("active");
+    } else {
+        skills.classList.remove("active");
+    }
+
+});
+
+window.addEventListener("scroll", function () {
+
+    let left = document.querySelector(".left-slide");
+    let right = document.querySelector(".right-slide");
+
+    let position = left.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight - 100;
+
+    if (position < screenPosition) {
+        left.classList.add("show");
+        right.classList.add("show");
+    }
+
+});
